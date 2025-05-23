138f522682f909afd9049fefe5dcdd9d54c779b5
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-05-23 13:13:54 +0300

    feat: add Product and ProductImage model and migration

---
+++ database/migrations/2025_05_23_100545_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->string('image_path');
+            $table->string('sku')->unique();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->string('image_path');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('product_images');
+    }
+};
