8c1dbc95764880cdd16703e01dcde481fb84c0a6
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-05-24 16:04:20 +0300

    feat: set default sorting for orders by creation date in OrderResource

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -41,6 +41,7 @@ class OrderResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->defaultSort('created_at', 'desc')
             ->columns([
                 Tables\Columns\TextColumn::make('user.name')
                     ->numeric()
